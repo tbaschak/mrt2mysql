15cbb77d4087267c26a8cc8592611528a38c2aa6
Theodore Baschak <theodore@ciscodude.net>
2019-01-21 23:02:53 -0600

    initial commit of code

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # mrt2mysql
 takes output from bgpscanner and converts it into mysql inserts
+
+## Usage
+
+This shell script loops through a large list of ASNs and pipes them into the script:
+
+```
+for as in `cat ca-asn-latest.txt`; do
+ echo "$as\$"
+ bgpscanner -p "$as\$" latest-bview | ./mrt2mysql-single.py
+done
+```
+
+
---
+++ mrt2mysql-batchedcommit.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import time
+import json
+import pymysql
+import IPy
+from datetime import datetime
+import re
+
+# When the parent dies we are seeing continual newlines, so we only access so many before stopping
+counter = 0
+sqlcounter = 0
+
+conn = pymysql.connect(
+    host='localhost',
+    user='user',
+    passwd='password',
+    db='db',
+)
+cur = conn.cursor()
+
+def getSourceASfromASPath(aspathstring):
+    if '{' not in aspathstring:
+        aspathlist = aspathstring.split(' ')
+        sourceasn = aspathlist[-1]
+    else:
+        aspathstring = re.sub(' \{.*\}$', '', aspathstring)
+        aspathlist = aspathstring.split(' ')
+        sourceasn = aspathlist[-1]
+    return int(sourceasn)
+
+def lineToMysql(oneline):
+    global sqlcounter
+    status,srcprefixes,aspath,stuff1,stuff2,stuff3,stuff4,communities,collector,timestamp,stuff5 = oneline.split('|')
+    sourceasn = getSourceASfromASPath(aspath)
+    ts = int(timestamp)
+    eventtime = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    prefixes = srcprefixes.split(' ')
+
+    for prefix in prefixes:
+        sqlinsert = """
+INSERT INTO mrt2mysql VALUES (NULL, '%s', '%s', '%s', %d)
+        """ % (eventtime, prefix, aspath, sourceasn)
+        #print sqlinsert
+        conn.ping(reconnect=True)
+        cur.execute(sqlinsert)
+        sqlcounter += 1
+        if sqlcounter == 501:
+            #conn.ping(reconnect=True)
+            conn.commit()
+            sqlcounter = 0
+
+
+while True:
+    try:
+        line = sys.stdin.readline().strip()
+        if line == "":
+            counter += 1
+            if counter > 100:
+                break
+            continue
+
+        counter = 0
+
+        lineToMysql(line)
+        #conn.commit()
+    except KeyboardInterrupt:
+        pass
+    except IOError:
+        # most likely a signal during readline
+        pass
+
